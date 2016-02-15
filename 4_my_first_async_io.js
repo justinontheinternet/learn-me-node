c63d099e5071a5028bd0e7a587f70e637b78201b
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-15 19:13:40 +0000

    exercise 4

--- 4_my_first_async_io.js
+++ 4_my_first_async_io.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var filepath = process.argv[2];
 
-var contents = fs.readFile(filepath, function(err, data){
+// because readFile is asynchronus, all manipulation must be done in the callback
+fs.readFile(filepath, function(err, data){
   if (!err) {
-    return data;
+    var bufferString = data.toString();
+    console.log(bufferString.split('\n').length - 1)
   }
 });
-
-console.log(contents.toString());
