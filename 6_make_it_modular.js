59278599e3ca089b42f4998b1e817b78ccdda802
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-15 23:48:52 +0000

    exercise 6

--- 6_make_it_modular.js
+++ 6_make_it_modular.js
@@ -1,4 +1,28 @@
 var myModule = require('./6_module');
-var fs = require('fs');
-var path = require('path');
+var dirPath = process.argv[2];
+var extension = process.argv[3];
 
+
+myModule(dirPath, extension, function(err, list) {
+
+  list.forEach(function (i) {
+    console.log(i);
+  });
+
+});
+
+
+
+
+// var filterFn = require('./solution_filter.js')
+// var dir = process.argv[2]
+// var filterStr = process.argv[3]
+
+// filterFn(dir, filterStr, function (err, list) {
+//   if (err)
+//     return console.error('There was an error:', err)
+
+//   list.forEach(function (file) {
+//     console.log(file)
+//   })
+// })
--- 6_module.js
+++ 6_module.js
@@ -1,6 +1,38 @@
 var fs = require('fs');
-var path = require('path');
-var filepath = process.argv[2];
-var extension = '.' + process.argv[3];
 
-fs.readdir(, function(err, list))
+
+module.exports = function(dir, extension, callback){
+  fs.readdir(dir, function(err, list){
+    if (err) 
+      return callback(err);
+    
+      var ext = '.' + extension;
+
+      var files = list.filter(function(file){
+        return file.indexOf(ext) !== -1;
+      });
+
+    callback(null, files);
+  });
+
+};
+
+
+
+// var fs = require('fs')
+// var path = require('path')
+
+// module.exports = function (dir, filterStr, callback) {
+
+//   fs.readdir(dir, function (err, list) {
+//     if (err)
+//       return callback(err)
+
+//     list = list.filter(function (file) {
+//       return path.extname(file) === '.' + filterStr
+//     })
+
+//     callback(null, list)
+//   })
+// }
+
