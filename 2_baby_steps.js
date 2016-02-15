52f920c6cc3ff7110a2ea83c8ac6e04e7daf891c
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-15 17:50:55 +0000

    exercise 2

--- 2_baby_steps.js
+++ 2_baby_steps.js
@@ -1,8 +1,8 @@
 var array = process.argv;
 var sum = 0;
 
-for ( var i = 2; i < array.length; i++ ) {
-  sum += array[i];
+for (var i = 2; i < array.length; i++) {
+  sum += Number(array[i]);
 }
 
-return sum;
+console.log(sum);
